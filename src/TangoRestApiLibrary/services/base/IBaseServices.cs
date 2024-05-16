4eeee4262a714b853499f030a42832b8e5b03005
AlejandropAxoft <alejandrop@axoft.com>
2024-05-16 09:48:20 -0300

    fix build

--- src/TangoRestApiLibrary/services/base/IBaseServices.cs
+++ src/TangoRestApiLibrary/services/base/IBaseServices.cs
@@ -9,7 +9,7 @@ namespace TangoRestApiClient.services.baseServices;
 /// <typeparam name="D">Dataset</typeparam>
 public interface IBaseServices<Q, D>
 {
-    Q GetData(); // devolver una colección del modelo de la query
+    Q GetData(int pageSize = 100, int pageIndex = 0); // devolver una colección del modelo de la query
     D GetDataById(int id); // devolver un objeto del modelo del ABM
     int GetIdByFilter(string filter); // ver
     TransactionResultModel Insert(D data); // devolver un INT con el ID
