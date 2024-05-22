ac6a5a72e0cea7dbf8029c0f4778286e92854b9e
AlejandropAxoft <alejandrop@axoft.com>
2024-05-22 17:15:42 -0300

    Agregado de proyecto de unittests

---
+++ src/TestProject1/PaisData.cs
@@ -0,0 +1,12 @@
+﻿using TangoRestApiLibrary.services.basemodel;
+
+namespace TestProject1
+{
+    internal class PaisData: BaseData
+    {
+        public int ID_PAIS { get; set; }
+        public string PAIS1 { get; set; }
+        public string COD_DGI { get; set; }
+        public string COD_AREA { get; set; }
+    }
+}
