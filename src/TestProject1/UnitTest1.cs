5826907916b5ad797ae4c790dc148e4176a0b931
AlejandropAxoft <alejandrop@axoft.com>
2024-05-24 11:58:18 -0300

    * Test Delete_Ok * Refactor, se marcan como static los métodos de implementació de asserts para explicitar los valores en el testcase

--- src/TestProject1/UnitTest1.cs
+++ src/TestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Moq;
 using Newtonsoft.Json;
+using System.Linq.Expressions;
 using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
@@ -12,14 +13,14 @@ namespace TestProject1
     public class UnitTest1
     {
         #region constantes para los tests
-        const string tangoUrl = "http://localhost:17123";
-        const string apiAuthorization = "C98BEF0B-F23F-4D0B-BBF3-49F4AF8F65DB";
-        const string companyId = "7";
-        const int processId = 1097;
-        const int paisId = 456;
-        const string nombrePais = "nombre del pais";
-        const string codigoDeArea = "código de area";
-        const string codigoDgi = "codigoDgi";
+        readonly string tangoUrl = "http://localhost:17123";
+        readonly string apiAuthorization = "C98BEF0B-F23F-4D0B-BBF3-49F4AF8F65DB";
+        readonly string companyId = "7";
+        readonly int processId = 1097;
+        readonly int paisId = 456;
+        readonly string nombrePais = "nombre del pais";
+        readonly string codigoDeArea = "código de area";
+        readonly string codigoDgi = "codigoDgi";
         #endregion
 
         private readonly Mock<IAxHttpClient> axHttpClientMock;
@@ -64,29 +65,64 @@ namespace TestProject1
             return task;
         }
 
-        private void SetUpPostAsync(PaisData paisData)
+        private static Task<HttpResponseMessage> GetHttpResponseMessage()
+        {
+            TransactionResultModel transactionResultModel = new()
+            {
+                Succeeded = true,
+            };
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(transactionResultModel), Encoding.UTF8, "application/json")
+            };
+            Task<HttpResponseMessage> task = Task.FromResult(responseMessage);
+            return task;
+        }
+
+        private static void SetUpHttpMethodAsync(Mock<IAxHttpClient> axHttpClientMock,
+            Expression<Func<IAxHttpClient, Task<HttpResponseMessage>>> expression,
+            int responseId,
+            string companyId, string apiAuthorization, string tangoUrl, string action, int processId, PaisData paisData)
         {
             axHttpClientMock
-                .Setup(m => m.PostAsync(It.IsAny<Uri>(), It.IsAny<HttpContent>()))
-                .Returns(GetHttpResponseMessage(paisId)) // seteo del response message con el paisId esperado
+                .Setup(expression)
+                .Returns(GetHttpResponseMessage(responseId)) // seteo del response message con el paisId esperado
                 .Callback(async (Uri uri, HttpContent content) =>
                 {
-                    HttpRequestMessage requestMessage = GetHttpRequestMessage(uri, content);
-                    HttpRequestHeaders headers = axHttpClientMock.Object.DefaultRequestHeaders;
-                    await CallbackAsserts(paisData, requestMessage, headers, "Create");
+                    await CallbackPostPutAsserts(axHttpClientMock, companyId, apiAuthorization, tangoUrl, action, processId, GetHttpRequestMessage(uri, content), paisData);
                 });
         }
 
-        private void SetUpPutAsync(PaisData paisData)
+        private static void SetUpPostAsync(Mock<IAxHttpClient> axHttpClientMock,
+            int responseId,
+            string companyId, string apiAuthorization, string tangoUrl, int processId, PaisData paisData)
+        {
+            SetUpHttpMethodAsync(axHttpClientMock,
+                m => m.PostAsync(It.IsAny<Uri>(), It.IsAny<HttpContent>()),
+                responseId,
+                companyId, apiAuthorization, tangoUrl, "Create", processId, paisData);
+        }
+
+        private static void SetUpPutAsync(Mock<IAxHttpClient> axHttpClientMock,
+            int responseId,
+            string companyId, string apiAuthorization, string tangoUrl, int processId, PaisData paisData)
+        {
+            SetUpHttpMethodAsync(axHttpClientMock,
+                m => m.PutAsync(It.IsAny<Uri>(), It.IsAny<HttpContent>()),
+                responseId,
+                companyId, apiAuthorization, tangoUrl, "Update", processId, paisData);
+        }
+
+        private static void SetUpDeleteAsync(Mock<IAxHttpClient> axHttpClientMock, string companyId, string apiAuthorization,
+            string tangoUrl, int processId, int paisId)
         {
             axHttpClientMock
-                .Setup(m => m.PutAsync(It.IsAny<Uri>(), It.IsAny<HttpContent>()))
-                .Returns(GetHttpResponseMessage(paisId)) // seteo del response message con el paisId esperado
-                .Callback(async (Uri uri, HttpContent content) =>
+                .Setup(m => m.DeleteAsync(It.IsAny<Uri>()))
+                .Returns(GetHttpResponseMessage())
+                .Callback((Uri uri) =>
                 {
-                    HttpRequestMessage requestMessage = GetHttpRequestMessage(uri, content);
-                    HttpRequestHeaders headers = axHttpClientMock.Object.DefaultRequestHeaders;
-                    await CallbackAsserts(paisData, requestMessage, headers, "Update");
+                    AssertHeaders(axHttpClientMock, companyId, apiAuthorization);
+                    Assert.Equal($"{tangoUrl}/api/Delete?process={processId}&id={paisId}", uri.AbsoluteUri); // URI es la url correcta según el config, action y process
                 });
         }
 
@@ -99,20 +135,28 @@ namespace TestProject1
             };
         }
 
-        private static async Task CallbackAsserts(PaisData paisData, HttpRequestMessage requestMessage, HttpRequestHeaders headers, string action)
+        private static async Task CallbackPostPutAsserts(Mock<IAxHttpClient> axHttpClientMock, string companyId, string apiAuthorization,
+            string tangoUrl, string action, int processId, HttpRequestMessage requestMessage,
+            PaisData paisData)
+        {
+            AssertHeaders(axHttpClientMock, companyId, apiAuthorization);
+            // Se verifica que cuando se llama al PostAsync sea con los parámetros correctos
+            Assert.Equal($"{tangoUrl}/api/{action}?process={processId}", requestMessage?.RequestUri?.AbsoluteUri); // URI es la url correcta según el config, action y process
+            Assert.Equal(JsonConvert.SerializeObject(paisData), await requestMessage.Content.ReadAsStringAsync()); // Content es un json del país que queremos dar dar de alta
+        }
+
+        private static void AssertHeaders(Mock<IAxHttpClient> axHttpClientMock, string companyId, string apiAuthorization)
         {
             // Verifico que al momento de ejecutar el post los headers sean los correctos
+            HttpRequestHeaders headers = axHttpClientMock.Object.DefaultRequestHeaders;
             Assert.Equal(companyId, headers.GetValues("Company").Single()); // El header contiene el CompanyId
-            Assert.Equal(apiAuthorization, headers.GetValues("ApiAuthorization").Single()); // El header contiene el ApiAuth                                       
-
-            // Se verifica que cuando se llama al PostAsync sea con los parámetros correctos
-            Assert.Equal($"{tangoUrl}/api/{action}?process={processId}", requestMessage.RequestUri.AbsoluteUri); // URI es la url correcta según el config, action y process
-            Assert.Equal(JsonConvert.SerializeObject(paisData), await requestMessage.Content.ReadAsStringAsync()); // Content es un json del país que queremos dar dar de alta
+            Assert.Equal(apiAuthorization, headers.GetValues("ApiAuthorization").Single()); // El header contiene el ApiAuth 
         }
         #endregion
 
+        #region Tests
         [Fact]
-        public void CreateExitoso()
+        public void Create_Ok()
         {
             // entidad que vamos a dar de alta
             PaisData paisData = new()
@@ -124,7 +168,8 @@ namespace TestProject1
 
             // setup del mock con el paisData que daremos de alta
             // contiene un callback con los asserts del header, uri y content del ApiPost
-            SetUpPostAsync(paisData);
+            // Los parámetros se corresponden con todos los datos que tienen un assert
+            SetUpPostAsync(axHttpClientMock, paisId, companyId, apiAuthorization, tangoUrl, processId, paisData);
 
             // instanciamos un paisService con el mock del axHttpClientMock
             PaisService paisService = new(tangoConfig, axHttpClientMock.Object);
@@ -138,7 +183,7 @@ namespace TestProject1
         }
 
         [Fact]
-        public void UpdateExitoso()
+        public void Update_Ok()
         {
             PaisData paisData = new()
             {
@@ -147,10 +192,26 @@ namespace TestProject1
                 COD_AREA = codigoDeArea,
                 COD_DGI = codigoDgi
             };
-            SetUpPutAsync(paisData);
+            SetUpPutAsync(axHttpClientMock, paisId, companyId, apiAuthorization, tangoUrl, processId, paisData);
             PaisService paisService = new(tangoConfig, axHttpClientMock.Object);
             paisService.Update(paisData);
             axHttpClientMock.Verify(m => m.PutAsync(It.IsAny<Uri>(), It.IsAny<HttpContent>()), Times.Once);
         }
+
+        [Fact]
+        public void Update_ErrorNoEsperado_IdDelResponseNoCoincide()
+        {
+            throw new NotImplementedException("Implementame porfa");
+        }        
+
+        [Fact]
+        public void Delete_Ok()
+        {
+            SetUpDeleteAsync(axHttpClientMock, companyId, apiAuthorization, tangoUrl, processId, paisId);
+            PaisService paisService = new(tangoConfig, axHttpClientMock.Object);
+            paisService.Delete(paisId);
+            axHttpClientMock.Verify(m => m.DeleteAsync(It.IsAny<Uri>()), Times.Once);
+        }
+        #endregion
     }
 }
