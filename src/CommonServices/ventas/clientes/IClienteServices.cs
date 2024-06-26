8ed2c5c6b5bb5216712920fcfc80cceb3ea8e3ad
Mauricio Ulla <mulla@ulasoft.com.ar>
2024-06-26 12:55:46 -0300

    Feature: Implementacion de IHasGetByCode en Clientes.

--- src/CommonServices/ventas/clientes/IClienteServices.cs
+++ src/CommonServices/ventas/clientes/IClienteServices.cs
@@ -1,8 +1,10 @@
 using TangoDeltaApi.CommonServices.ventas.cliente.model;
 using TangoDeltaApi.Core.Service;
+using TangoDeltaApiCore.Service;
 
 namespace TangoDeltaApi.CommonServices.ventas.cliente;
 
 public interface IClienteServices: IBaseServices<ClienteQueryRecord, ClienteData>
+    , IHasGetByCode<string, ClienteQueryRecord>
 {
 }
