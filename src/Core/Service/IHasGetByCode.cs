9673cb100ab4e657f2d9d6c4aeba75635dd99f6a
Mauricio Ulla <mulla@ulasoft.com.ar>
2024-06-26 12:54:39 -0300

    Feature: Nueva interface para GetByCode()

---
+++ src/Core/Service/IHasGetByCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TangoDeltaApi.Core.Service;
+
+namespace TangoDeltaApiCore.Service
+{
+    public interface IHasGetByCode<in T, out QR>
+        where T : IConvertible
+        where QR : BaseQueryRecord
+    {
+
+        /// <summary>
+        /// Permite obtener un registro a partir de su código
+        /// de Identificador alternativo UniqueKey.
+        /// </summary>
+        /// <param name="code">
+        /// código de la entidad a utilizar como filtro.
+        /// </param>
+        /// <returns>
+        /// Instancia de la entidad.
+        /// </returns>
+        QR GetByCode(T code);
+
+        /// <summary>
+        /// Permite obtener el Id de un registro a partir de su código
+        /// de Identificador alternativo UniqueKey.
+        /// </summary>
+        /// <param name="code">
+        /// Código de la entidad a utilizar como filtro.
+        /// </param>
+        /// <returns>
+        /// Id de la entidad.
+        /// </returns>
+        int GetIdByCode(T code);
+
+    }
+}
