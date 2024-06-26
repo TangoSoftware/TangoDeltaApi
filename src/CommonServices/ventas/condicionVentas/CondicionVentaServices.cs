69ff0afa98c43e873f8fbf7f7aa112030f77b7de
Mauricio Ulla <mulla@ulasoft.com.ar>
2024-06-26 13:11:14 -0300

    Feature: Implementacion de IHasGetByCode en CondicionDeVenta.

--- src/CommonServices/ventas/condicionVentas/CondicionVentaServices.cs
+++ src/CommonServices/ventas/condicionVentas/CondicionVentaServices.cs
@@ -8,4 +8,14 @@ public class CondicionVentaServices(ITangoConfig config)
     : BaseServices<CondicionVentaQueryRecord, CondicionVentaData>(config), ICondicionVentaServices
 {
     protected override string ProcessId => "2497";
+
+    public CondicionVentaQueryRecord GetByCode(int code)
+    {
+        return GetByFilter($"GVA01.COND_VTA = {code}").Single();
+    }
+
+    public int GetIdByCode(int code)
+    {
+        return GetByCode(code).IdGva01;
+    }
 }
