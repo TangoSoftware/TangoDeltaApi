6dba88ff0f2dabccec5e34727e76fc042428e95b
Mauricio Ulla <mulla@ulasoft.com.ar>
2024-06-26 13:24:45 -0300

    Feature: Implementacion de IHasGetByCode en ClasificacionDeComprobantes.

--- src/CommonServices/procesosgenerales/clasificaciondecomprobantes/IClasificacionDeComprobantesServices.cs
+++ src/CommonServices/procesosgenerales/clasificaciondecomprobantes/IClasificacionDeComprobantesServices.cs
@@ -1,8 +1,10 @@
 using TangoDeltaApi.CommonServices.procesosgenerales.ClasificacionDeComprobantes.model;
 using TangoDeltaApi.Core.Service;
+using TangoDeltaApiCore.Service;
 
 namespace TangoDeltaApi.CommonServices.procesosgenerales.ClasificacionDeComprobantes;
 
 public interface IClasificacionDeComprobantesServices : IBaseServices<ClasificacionDeComprobantesQueryRecord, ClasificacionDeComprobantesData>
+    , IHasGetByCode<string, ClasificacionDeComprobantesQueryRecord>
 {
 }
