9a0ce620a206b49fb0ec225852e6fd9974288a01
Mauricio Ulla <mulla@ulasoft.com.ar>
2024-06-26 13:18:50 -0300

    Feature: Implementacion de IHasGetByCode en Transporte.

--- src/CommonServices/ventas/transporte/TransporteServices.cs
+++ src/CommonServices/ventas/transporte/TransporteServices.cs
@@ -8,4 +8,14 @@ public class TransporteServices(ITangoConfig config)
     : BaseServices<TransporteQueryRecord, TransporteData>(config), ITransporteServices
 {
     protected override string ProcessId => "960";
+
+    public TransporteQueryRecord GetByCode(string code)
+    {
+        return GetByFilter($"GVA24.cod_transp = '{code}'").Single();
+    }
+
+    public int GetIdByCode(string code)
+    {
+        return GetByCode(code).IdGva24;
+    }
 }
