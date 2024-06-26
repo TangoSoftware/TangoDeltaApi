fd83cd4aba65cffde7b281a654bc6fb34b9a87cb
Mauricio Ulla <mulla@ulasoft.com.ar>
2024-06-26 13:21:29 -0300

    Feature: Implementacion de IHasGetByCode en Vendedor.

--- src/CommonServices/ventas/vendedor/IVendedorServices.cs
+++ src/CommonServices/ventas/vendedor/IVendedorServices.cs
@@ -1,8 +1,10 @@
 using TangoDeltaApi.CommonServices.ventas.vendedor.model;
 using TangoDeltaApi.Core.Service;
+using TangoDeltaApiCore.Service;
 
 namespace TangoDeltaApi.CommonServices.ventas.vendedor;
 
 public interface IVendedorServices: IBaseServices<VendedorQueryRecord, VendedorData>
+    , IHasGetByCode<string, VendedorQueryRecord>
 {
 }
--- src/CommonServices/ventas/vendedor/VendedorServices.cs
+++ src/CommonServices/ventas/vendedor/VendedorServices.cs
@@ -8,4 +8,14 @@ public class VendedorServices(ITangoConfig config)
     : BaseServices<VendedorQueryRecord, VendedorData>(config), IVendedorServices
 {
     protected override string ProcessId => "952";
+
+    public VendedorQueryRecord GetByCode(string code)
+    {
+        return GetByFilter($"GVA23.COD_VENDED = '{code}'").Single();
+    }
+
+    public int GetIdByCode(string code)
+    {
+        return GetByCode(code).IdGva23;
+    }
 }
