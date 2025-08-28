2576ba650c8d41e6035018d2d4c8f9c96fa63c12
Jonathan Ponce <jponce@axoft.com>
2025-08-28 15:47:00 -0300

    * Se agrega proyecto Tiendas Api Console

---
+++ src/Implementations/TiendasApiConsole/PriceListResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace TiendasApiConsole
+{
+    // Clase para representar la respuesta de la API que contiene una lista de PriceList y metadatos de paginación
+    public class PriceListResponse
+    {
+        public List<PriceList>? Data { get; set; }
+
+        public int pageNumber;
+        public int pageSize;
+        public bool hasNextPage;
+        public List<string>? message; // O hay pricelist o hay message
+        public OrderErrorDto orderError;
+        public bool Succeeded;
+    }
+
+    // Clase para representar errores específicos relacionados con órdenes
+    public class OrderErrorDto
+    {
+        public string Orders { get; set; }
+
+        public string Message { get; set; }
+    }
+}
