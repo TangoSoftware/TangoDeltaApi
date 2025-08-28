72740b686bf8a03774c151f04d39cb26f3733e72
Jonathan Ponce <jponce@axoft.com>
2025-08-28 14:46:53 -0300

    * Se agrega TiendasApiConsole

---
+++ src/Implementations/TiendasApiConsole/PriceList.cs
@@ -0,0 +1,23 @@
+﻿namespace TiendasApiConsole
+{
+    public class PriceList
+    {
+        public int Id { get; set; }
+
+        public int PriceListNumber { get; set; }
+
+        public string Description { get; set; }
+
+        public bool CommonCurrency { get; set; }
+
+        public bool IvaIncluded { get; set; }
+
+        public bool InternalTaxIncluded { get; set; }
+
+        public DateTime? ValidityDateSince { get; set; }
+
+        public DateTime? ValidityDateUntil { get; set; }
+
+        public bool Enabled { get; set; }
+    }
+}
